26b793702fdef8c59dee50817aa272425e38fc9f
rikukuikka <113688063+rikukuikka@users.noreply.github.com>
2022-09-16 20:54:30 +0300

    Added enemies and lasers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            fireLaser();
+        }
     }
 
     void CalculateMovement()
@@ -25,7 +36,7 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        
+
         transform.Translate(direction * _speed * Time.deltaTime);
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
@@ -37,6 +48,22 @@ public class Player : MonoBehaviour
         else if (transform.position.x < -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void fireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Vector3 offset = new Vector3(0, 0.8f, 0);
+        Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+    }
+
+    public void Damage() 
+    {
+        _lives--;
+        if(_lives < 1)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
